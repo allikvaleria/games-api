dd19c8eaacd85c8b485faac4042831ae42e10017
Valeria <allikvaleria@gmail.com>
2025-10-10 12:27:19 +0300

    Get full details about the games

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
 const games = [
-    "Witcher 3",
-    "Cyberpunk 2077",
-    "Minecraft",
-    "Counter-Strike: Global Offensive",
-    "Roblox",
-    "Grand Theft Auto V",
-    "Valoorant",
-    "Forza Horizon 5"
+    {id: 1, name: "Witcher 3", price: 29.99},
+    {id: 2, name: "Cyberpunk 2077", price: 59.99},
+    {id: 3, name: "Minecraft", price: 26.99},
+    {id: 4, name: "Counter-Strike: Global Offensive", price: 0},
+    {id: 5, name: "Roblox", price: 0},
+    {id: 6, name: "Grand Theft Auto V", price: 29.99},
+    {id: 7, name: "Valorant", price: 0},
+    {id: 8, name: "Forza Horizon 5", price: 59.99},
 ]
 app.get('/games', (req, res) => {
     res.send(games)
