494c7223e425e265507592e658706511869775a3
Valeria <allikvaleria@gmail.com>
2025-10-10 12:20:12 +0300

    End point for data collection

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+const games = [
+    "Witcher 3",
+    "Cyberpunk 2077",
+    "Minecraft",
+    "Counter-Strike: Global Offensive",
+    "Roblox",
+    "Grand Theft Auto V",
+    "Valoorant",
+    "Forza Horizon 5"
+]
 app.get('/games', (req, res) => {
-    res.send(["Witcher 3", "Cyberpunk 2077"])
+    res.send(games)
 })
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.get('/game/:id', (req, res) => {
+    res.send(games[req.params.id])
+})
+app.use('/docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
+
+
 })
